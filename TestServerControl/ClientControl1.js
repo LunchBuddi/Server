f0dd4400c8ed70744f03d2d1a9f05dbb212690a2
Jack Brown <jack.brown.255@gmail.com>
2015-10-15 19:16:46 -0500

    Created jsut a basic, unedited AJAX Server Control to play around with.

---
+++ TestServerControl/ClientControl1.js
@@ -0,0 +1,23 @@
+﻿/// <reference name="MicrosoftAjax.js"/>
+
+
+Type.registerNamespace("TestServerControl");
+
+TestServerControl.ClientControl1 = function(element) {
+    TestServerControl.ClientControl1.initializeBase(this, [element]);
+}
+
+TestServerControl.ClientControl1.prototype = {
+    initialize: function() {
+        TestServerControl.ClientControl1.callBaseMethod(this, 'initialize');
+        
+        // Add custom initialization here
+    },
+    dispose: function() {        
+        //Add custom dispose actions here
+        TestServerControl.ClientControl1.callBaseMethod(this, 'dispose');
+    }
+}
+TestServerControl.ClientControl1.registerClass('TestServerControl.ClientControl1', Sys.UI.Control);
+
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
